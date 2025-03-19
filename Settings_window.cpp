6912ff7580e64f9f783e8dd187d2a357e48118e9
matthew1115 <matthew_1115@outlook.com>
2025-03-19 14:57:23 +0800

    small feature - show the settings file in the settings window

--- Settings_window.cpp
+++ Settings_window.cpp
@@ -1,5 +1,6 @@
 #include "Settings_window.h"
 #include <fstream>
+#include <qdebug>
 
 SettingsWindow::SettingsWindow(QWidget* parent, Random_picker* main_win_ptr)
 	: QDialog(parent)
@@ -11,6 +12,23 @@ SettingsWindow::SettingsWindow(QWidget* parent, Random_picker* main_win_ptr)
 
 	connect(setting_ui.buttonBox, SIGNAL(accepted()), this, SLOT(on_buttonBox_accepted()));
 	connect(setting_ui.buttonBox, SIGNAL(rejected()), this, SLOT(on_buttonBox_rejected()));
+
+	std::fstream settings_file("settings.txt", std::ios::in);
+	if (settings_file.is_open())
+	{
+		int min, max;
+		std::string exclude_str;
+		settings_file >> min >> max;
+		settings_file.ignore();
+		std::getline(settings_file, exclude_str);
+		setting_ui.spinBox_min->setValue(min);
+		setting_ui.spinBox_max->setValue(max);
+		setting_ui.plainTextEdit_exclude->setPlainText(QString::fromStdString(exclude_str));
+	}
+	else
+	{
+		qDebug() << "Settings file not found";
+	}
 }
 
 SettingsWindow::~SettingsWindow()
